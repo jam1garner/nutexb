9a10a439b8259bc1f1b2b68384a7377783b46f74
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2021-05-12 11:15:12 -0500

    update binread, improve nutexb data parsing performance

--- src/parser.rs
+++ src/parser.rs
@@ -1,45 +1,47 @@
 use binread::io::SeekFrom;
 use binread::prelude::*;
 use binread::NullString;
-
+use binread::helpers::read_bytes;
 use binread::BinResult;
 
 #[derive(BinRead, Debug, Clone)]
 pub struct NutexbFile {
-    #[br(seek_before = SeekFrom::End(-8))]
+    #[br(seek_before = SeekFrom::End(-112))]
     pub footer: NutexbFooter,
 
-    #[br(seek_before = SeekFrom::Start(0), count = footer.size)]
+    // Specify the parse function to avoid reading bytes individually.
+    #[br(seek_before = SeekFrom::Start(0), parse_with = read_bytes, count = footer.size)]
     pub data: Vec<u8>,
 }
 
 #[derive(BinRead, Debug, Clone)]
-#[br(magic = b" XET")]
+#[br(magic = b" XNT")]
 pub struct NutexbFooter {
-    pub version: (u16, u16),
+    #[br(map = NullString::into_string)]
+    pub string: String,
 
-    #[br(seek_before = SeekFrom::End(-0x2C))]
+    #[br(seek_before = SeekFrom::End(-44))]
     pub width: u32,
     pub height: u32,
     pub depth: u32,
-
+    
     #[br(map = u8::into)]
     pub image_format: NutexbFormat,
-
+    
     #[br(pad_after = 0x2)]
     pub unk: u8, // 4?
-
+    
     pub unk2: u32,
     pub mip_count: u32,
     pub alignment: u32,
     pub array_count: u32,
     pub size: u32,
 
-    #[br(seek_before = SeekFrom::End(-0xB0), count = mip_count)]
-    pub mip_sizes: Vec<u32>,
+    #[br(magic = b" XET")]
+    pub version: (u16, u16),
 
-    #[br(seek_before = SeekFrom::End(-0x70), magic = b" XNT", map = NullString::into_string)]
-    pub string: String,
+    #[br(seek_before = SeekFrom::End(-176), count = mip_count)]
+    pub mip_sizes: Vec<u32>,
 }
 
 pub fn read_nutexb(path: &std::path::Path) -> Result<NutexbFile, Box<dyn std::error::Error>> {
