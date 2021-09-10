348070843b06f63ed95a55289a21c29f42fb1c04
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2021-09-09 21:23:54 -0500

    add bpp for nutexb formats

--- src/lib.rs
+++ src/lib.rs
@@ -27,3 +27,34 @@ pub enum NutexbFormat {
     BC7Unorm = 224,
     BC7Srgb = 229,
 }
+
+impl NutexbFormat {
+    /// The number of bytes to store a single pixel.
+    /// For block compressed formats like [NutexbFormat::BC7Srgb], this is the size in bytes of a single block.
+    /**
+    ```rust
+    assert_eq!(4, NutexbFormat::R8G8B8A8Unorm.size_in_bytes());
+    assert_eq!(8, NutexbFormat::BC1Unorm.size_in_bytes());
+    assert_eq!(16, NutexbFormat::BC7Unorm.size_in_bytes());
+    assert_eq!(16, NutexbFormat::R32G32B32A32Float.size_in_bytes());
+    ```
+    */
+    pub fn bytes_per_pixel(&self) -> u32 {
+        match &self {
+            NutexbFormat::R8G8B8A8Unorm
+            | NutexbFormat::R8G8B8A8Srgb
+            | NutexbFormat::B8G8R8A8Unorm
+            | NutexbFormat::B8G8R8A8Srgb => 4,
+            NutexbFormat::R32G32B32A32Float => 16,
+            NutexbFormat::BC1Unorm | NutexbFormat::BC1Srgb => 8,
+            NutexbFormat::BC2Unorm | NutexbFormat::BC2Srgb => 16,
+            NutexbFormat::BC3Unorm | NutexbFormat::BC3Srgb => 16,
+            NutexbFormat::BC4Unorm | NutexbFormat::BC4Snorm => 8,
+            NutexbFormat::BC5Unorm => 16,
+            NutexbFormat::BC5Snorm => 16,
+            NutexbFormat::BC6Ufloat => 16,
+            NutexbFormat::BC7Unorm => 16,
+            NutexbFormat::BC7Srgb => 16,
+        }
+    }
+}
